1dfae993195947b1975ae2e850237832d330c7a8
Jimmy Mintzer <smashin109@gmail.com>
2014-12-06 22:07:55 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,4 @@
+var React = require('react');
+var ReacterNewsApp = require('./components/ReacterNewsApp');
+
+//React.render(<ReacterNewsApp />, document.getElementById('app'));
---
+++ src/components/ReacterNewsApp.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var ReacterNewsApp = React.createClass({
+  render: function() {
+    return (
+      <h1>Hello from ReacterNewsApp</h1>
+    )
+  }
+});
+
+module.exports = ReacterNewsApp;
